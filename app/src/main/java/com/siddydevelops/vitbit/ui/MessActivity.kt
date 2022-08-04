c39b802b0c79e417b9e76822beab3987b2f486c6
SiddyDevelops <siddhartsingh2002@gmail.com>
2022-08-04 14:22:15 +0530

    Added AugustSchedule

--- app/src/main/java/com/siddydevelops/vitbit/ui/MessActivity.kt
+++ app/src/main/java/com/siddydevelops/vitbit/ui/MessActivity.kt
@@ -132,7 +132,7 @@ class MessActivity : AppCompatActivity() {
             val inputStream = assetManager.open("mess_schedule.xls")
             val workbook = Workbook.getWorkbook(inputStream)
             val sheet = workbook.getSheet(0)                        //Two sheets per tt
-            val rows = 7
+            val rows = sheet.rows
             val col = sheet.columns
             for (j in 0 until rows - 1) {
                 val cell = sheet.getCell(0, j + 1)       //Date and Days
